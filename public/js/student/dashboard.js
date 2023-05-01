1cbcf79cd84ecd8b6537eea4daa1f692637ff0a1
HetPatel9 <het.p@simformsolutions.com>
2023-05-01 18:10:07 +0530

    Integration of APIs with Backend

---
+++ public/js/student/dashboard.js
@@ -0,0 +1,7 @@
+async function INIT() {
+    // Also get Student info to Show in the Header (Atleast FirstName)
+    // Setting it in cookies will also work
+    // Make 1 API call to fetch info of every subject
+}
+
+INIT();
