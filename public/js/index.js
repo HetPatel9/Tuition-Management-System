44ccd5084a0ca4c093a92b24945abb69f6109c03
HetPatel9 <het.p@simformsolutions.com>
2023-05-02 17:59:22 +0530

    styling is completed

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,11 @@ let boxLength = container.offsetWidth;
 const leftArrow = document.querySelector('#left-arrow');
 
 leftArrow.addEventListener('click', () => {
-    console.log(boxLength);
-    container.scrollBy({
+        container.scrollBy({
         left: boxLength * -0.5,
         behavior: "smooth"
     })
-    // This condition makes scrolling circular 
+  
     if (container.scrollLeft === 0) {
         container.scrollTo({
             left: container.scrollWidth,
@@ -23,7 +22,7 @@ rightArrow.addEventListener('click', () => {
         behavior: "smooth"
     })
 
-    // This condition makes scrolling circular
+
     if (container.scrollLeft > container.offsetWidth) {
         container.scrollTo({
             left: 0,
